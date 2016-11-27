36b8fc6a50a6bd7014769aeeaef9949df83749cd
git2tan <git2tan@mail.ru>
2016-11-27 14:54:43 +0700

    some changes

--- oop_lab3_var2_Turchin/Book.cpp
+++ oop_lab3_var2_Turchin/Book.cpp
@@ -11,11 +11,7 @@ Book::Book() :Title("<empty>"+ std::to_string(objCount)), Author("<empty>"+ std:
 	std::cout << "Конструктор для " << std::endl;
 	this->printBook();
 }
-//Book::Book(std::string Title, std::string Author, bool inLib) : Title(Title), Author(Author), inLib(inLib)
-//{
-//	checkArray();						//проверяем размер массива под указатели на объекты
-//	Array[objCount++] = this;			////вставляем указатель на только что созданный объект, в массив указателей
-//}
+
 Book::~Book()
 {
 	this->Title.clear();
@@ -59,49 +55,6 @@ void Book::printBook() const										//Статический метод вывода объекта на экра
 	std::cout << std::setw(20) << std::left << this->Title
 		<< std::setw(20) << std::left << this->Author
 		<< (inLib ? "YES" : "NO") << std::endl;
-}
-
-Book &Book::setBook(std::string Title, std::string Author, bool inLib)
-{
-	this->Title = Title;
-	this->Author = Author;
-	this->inLib = inLib;
-	return *this;
-}
-
-Book &Book::setInLib(bool inLib)
-{
-	this->inLib = inLib;
-	return *this;
-}
-Book &Book::setTitle(std::string Title)
-{
-	if (!Title.empty())
-		this->Title = Title;
-	else
-		this->Title = "<empty(default)>";
-	return *this;
-}
-Book &Book::setAuthor(std::string Author)
-{
-	if (!Author.empty())
-		this->Author = Author;
-	else
-		this->Author = "<no name>";
-	return *this;
-}
-
-const bool Book::getInLib() const
-{
-	return this->inLib;
-}
-const std::string Book::getTitle() const
-{
-	return this->Title;
-}
-const std::string Book::getAuthor() const
-{
-	return this->Author;
 }
 
 void Book::showArray()
